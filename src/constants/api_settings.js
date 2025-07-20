75222505de4adbfe856a2b05f44df63a657e2657
Vandana Premarajan <vandana1311@gmail.com>
2025-07-20 12:52:41 +0400

    vercel url updated

--- src/constants/api_settings.js
+++ src/constants/api_settings.js
@@ -1,4 +1,11 @@
-export const Base_URL = 'http://localhost:5050/api/';
-export const APP_URL = 'http://localhost:5050/';
+//local server
+//export const Base_URL = 'http://localhost:5050/api/';
+//export const APP_URL = 'http://localhost:5050/';
+
+
+//live server
+export const Base_URL = 'https://netcliq-back.vercel.app/api/';
+export const APP_URL = 'https://netcliq-back.vercel.app/';
+
 export const TOKEN_HEADER_KEY = 'abc1_token_header_key';
 export const Token_name = 'authToken';
